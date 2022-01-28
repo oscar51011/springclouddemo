33b42cdac49d1c9f72a2aa21187d3338f270eae8
oscar51011 <oscar51011@gmail.com>
2022-01-28 23:38:46 +0800

    spring cloud demo project init

---
+++ payment/src/main/java/com/demo/payment/PaymentApplication.java
@@ -0,0 +1,20 @@
+package com.demo.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 新增 payment 啟動類
+ * @author oscar51011
+ * @date 2022年1月28日
+ *
+ */
+
+@SpringBootApplication
+public class PaymentApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(PaymentApplication.class, args);
+	}
+
+}
